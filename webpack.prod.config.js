11d393d08d73e430250ca6170238370cbd5b7dfd
bumblebee24 <martina.prgomet@gmail.com>
2016-10-28 14:39:30 +0200

    setup circle ci

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -21,8 +21,8 @@ module.exports = {
     }),
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify('production'),
-      __APP_URL__: JSON.stringify('http://192.168.50.4:7000'),
-      __API_URL__: JSON.stringify('http://192.168.50.4:3000'),
+      __APP_URL__: JSON.stringify(process.env.APP_URL),
+      __API_URL__: JSON.stringify(process.env.API_URL),
     }),
     new webpack.optimize.CommonsChunkPlugin('common-[chunkhash].js'),
     new webpack.optimize.DedupePlugin(),
