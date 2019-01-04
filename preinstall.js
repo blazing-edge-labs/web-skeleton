9fec2847716ff0ca7b9e119f086a5b2f9bafecc2
Robert Katić <robert@blazingedge.io>
2019-01-05 00:27:23 +0100

    Remove preinstall script

--- preinstall.js
+++
@@ -1,7 +0,0 @@
-'use strict'
-
-// Detect if run by NPM and fail
-if (process.env.npm_lifecycle_script) {
-  console.log('---- This project uses yarn! Run "yarn install" instead. ----\n')
-  process.exit(1)
-}
